bf185782cf302c28db65bf49b7fad2a0abf6a20a
Douglas Leal <douglas.leal@live.com>
2023-01-30 23:45:54 -0300

    Menu criado

--- EditorDeTextos/Program.cs
+++ EditorDeTextos/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Menu();
+
+static void Menu()
+{
+    Console.Clear();
+    Console.WriteLine("Editor de Texto");
+    Console.WriteLine("---------------");
+    Console.WriteLine("1 - Abrir arquivo");
+    Console.WriteLine("2 - Criar arquivo");
+    Console.WriteLine();
+    Console.WriteLine("0 - Sair");
+    Console.WriteLine();
+
+    Console.Write("Opção: ");
+    short option = short.Parse(Console.ReadLine());
+
+    switch (option)
+    {
+        case 0: Environment.Exit(0); break;
+        case 1: Abrir(); break;
+        case 2: Criar(); break;
+        default: Menu(); break;
+    }
+}
+
+static void Abrir()
+{
+
+}
+
+static void Criar()
+{
+
+}
