64a3093773a448eeb2f09c57d54cdf85abb8c55e
Douglas Leal <douglas.leal@live.com>
2023-02-01 18:50:54 -0300

    Projeto concluído

--- EditorDeTextos/Program.cs
+++ EditorDeTextos/Program.cs
@@ -1,4 +1,8 @@
-﻿Menu();
+﻿using System.IO;
+using System.Text.RegularExpressions;
+using static System.Net.Mime.MediaTypeNames;
+
+Menu();
 
 static void Menu()
 {
@@ -25,10 +29,63 @@ static void Menu()
 
 static void Abrir()
 {
+    Console.Clear();
+    Console.WriteLine("Digite o caminho do arquivo.");
 
+    Console.Write("Caminho: ");
+    var pathname = Console.ReadLine();
+
+    using (var file = new StreamReader(pathname))
+    {
+        string text = file.ReadToEnd();
+        Console.WriteLine(text);
+    }
+
+    Console.WriteLine();
+    Console.WriteLine("Aperte [ENTER] para continuar.");
+
+    Console.ReadKey();
+
+    Menu();
 }
 
 static void Criar()
 {
+    Console.Clear();
+    Console.WriteLine("Digite o texto abaixo ([ESC] para sair)");
+    Console.WriteLine("---------------------");
+    Console.WriteLine();
 
+    string text = "";
+
+    do 
+    {
+        text += Console.ReadLine();
+        text += Environment.NewLine;
+    } 
+    while (Console.ReadKey().Key != ConsoleKey.Escape);
+
+    Salvar(text);
+}
+
+static void Salvar(string text)
+{
+    Console.Clear();
+
+    Console.WriteLine("Qual o caminho para salvar o arquivo?");
+    Console.Write("Caminho: ");
+    var pathname = Console.ReadLine();
+
+    using (var file = new StreamWriter(pathname))
+    {
+        file.Write(text);
+    }
+
+    Console.WriteLine($"arquivo {pathname} salvo com sucesso");
+    Console.WriteLine();
+    Console.WriteLine("Aperte [ENTER] para continuar.");
+
+    Console.ReadKey();
+
+    Menu();
 }
